fdde9fa48444d255eb08ac8a8570f16765ee1735
Cole Kembel <cokembel@mines.edu>
2012-11-27 01:42:15 -0700

    Integrated farm name and row number into activity_main.xml

--- FarmHand/gen/com/kembel/farmhand/R.java
+++ FarmHand/gen/com/kembel/farmhand/R.java
@@ -15,7 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int farm_name=0x7f070000;
+        public static final int menu_settings=0x7f070004;
+        public static final int next_row=0x7f070003;
+        public static final int prev_row=0x7f070001;
+        public static final int row_number=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,9 +29,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int farm_name=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
