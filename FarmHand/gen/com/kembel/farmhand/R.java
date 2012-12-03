72dfd698b15fb4ec00a2a077f2b8e44122b581f5
Cole Kembel <cokembel@mines.edu>
2012-12-03 02:00:16 -0700

    Modified acivity_main.xml to use relative layout

--- FarmHand/gen/com/kembel/farmhand/R.java
+++ FarmHand/gen/com/kembel/farmhand/R.java
@@ -10,30 +10,41 @@ package com.kembel.farmhand;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int red=0x7f040000;
+    }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int borders=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int farm_name=0x7f070000;
-        public static final int menu_settings=0x7f070004;
-        public static final int next_row=0x7f070003;
-        public static final int prev_row=0x7f070001;
-        public static final int row_number=0x7f070002;
+        public static final int current_status=0x7f080004;
+        public static final int down_button=0x7f080005;
+        public static final int farm_label=0x7f080002;
+        public static final int farm_name=0x7f080000;
+        public static final int menu_settings=0x7f080009;
+        public static final int next_row_button=0x7f080007;
+        public static final int not_down_button=0x7f080006;
+        public static final int prev_row_button=0x7f080001;
+        public static final int row_number=0x7f080008;
+        public static final int status_label=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int farm_name=0x7f040003;
-        public static final int menu_settings=0x7f040001;
-        public static final int title_activity_main=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int farm_name=0x7f050003;
+        public static final int menu_settings=0x7f050001;
+        public static final int status=0x7f050004;
+        public static final int title_activity_main=0x7f050002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
